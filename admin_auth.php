ffe2cb19af0bdb4bfc449e1806941790420a156a
HotPepper221 <lord-0999@mail.ru>
2016-02-23 23:55:07 +1000

    admin_auth.php
    admin_auth.php

--- admin_auth.php
+++ admin_auth.php
@@ -1,21 +1,28 @@
-<?php
-    session_start();
-	include("db.php");
+<title>Авторизация</title>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+        <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-pink.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+        <link rel="stylesheet" href="./assets/css/main.css">
 
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	
-	if($username == $admin_user) {
-	   if($password == $admin_password) {
-		   $_SESSION['admin'] = $admin_user;
-           $_SESSION['username'] = $admin_user;
-           echo '<script>document.location.href = "/admin.php"</script>';
-	   }
-       else {
-           echo '<br><br><center><h2>Неправильный логин и/или пароль</center></h2><script>setTimeout("document.location.href = \'/admin.php\'", 2000);</script>';   
-       }
-    }
-    else {
-        echo '<br><br><center><h2>Неправильный логин и/или пароль</center></h2><script>setTimeout("document.location.href = \'/admin.php\'", 2000);</script>';   
-    }
-?>
+<div class="auth-form">
+            <div class="mdl-grid">
+                <form class="mdl-grid mdl-cell--8-col mdl-shadow--2dp" method="post" action="./assets/php_proccess/admin_auth_pr.php">
+                    <h3>Вход</h3>
+                    <div class="mdl-textfield mdl-js-textfield mdl-cell-12-col">
+                        <input class="mdl-textfield__input" type="text" id="username" name="username" required />
+                        <label class="mdl-textfield__label" for="username">Логин</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield mdl-cell-12-col">
+                        <input class="mdl-textfield__input" type="password" id="password" name="password" required />
+                        <label class="mdl-textfield__label" for="password">Пароль</label>
+                    </div>
+                    <button type="submit" class="mdl-button mdl-js-button mdl-button--primary">
+                        Войти
+                    </button>
+                </form>
+            </div>
+        </div>
+</html>
