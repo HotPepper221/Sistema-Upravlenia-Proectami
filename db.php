93eb9563f6c96bc3b0982368b45d01284b835238
jusTIZy <kamihds@bk.ru>
2016-02-23 23:54:41 +1000

    db.php
    process

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+    $server = 'localhost';
+    $db_user = 'root';
+    $db_pwd = '';
+    $db_name = 'project';
+    $users_table_name = 'users';
+    $projects_table_name = 'projects';
+    $log_table_name = 'log';
+    $files_table_name = 'files';
+    $admin_user = 'admin';
+    $admin_password = 'pass';
+?>
